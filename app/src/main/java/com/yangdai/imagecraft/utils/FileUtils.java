e7153ba2da99839d1cd9cf268d6034cefda0e909
30415 <dy15800837435@gmail.com>
2023-10-09 00:04:42 +0200

    fix bugs

--- app/src/main/java/com/yangdai/imagecraft/utils/FileUtils.java
+++ app/src/main/java/com/yangdai/imagecraft/utils/FileUtils.java
@@ -1,9 +1,12 @@
 package com.yangdai.imagecraft.utils;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 
 import com.yangdai.imagecraft.R;
 
@@ -22,21 +25,46 @@ public class FileUtils {
      * 获取真实路径
      */
     public static String getRealPathFromUri(Uri uri, Context context) {
-        Cursor cursor = context.getContentResolver().query(uri, null, null, null);
-        if (cursor != null) {
-            try {
+        String filePath = "";
+        String scheme = uri.getScheme();
+        if (scheme == null)
+            filePath = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            filePath = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            if (cursor != null) {
                 if (cursor.moveToFirst()) {
-                    int idx = cursor.getColumnIndex("_data");
-                    return cursor.getString(idx);
+                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    filePath = cursor.getString(columnIndex);
                 }
-            } finally {
                 cursor.close();
             }
+            if (TextUtils.isEmpty(filePath)) {
+                filePath = getFilePathForNonMediaUri(context, uri);
+            }
         }
-        return uri.getPath();
+        return filePath;
     }
 
-    /** @noinspection ResultOfMethodCallIgnored*/
+    //非媒体文件中查找
+    private static String getFilePathForNonMediaUri(Context context, Uri uri) {
+        String filePath = "";
+        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow("_data");
+                filePath = cursor.getString(columnIndex);
+            }
+            cursor.close();
+        }
+        return filePath;
+    }
+
+    /**
+     * @noinspection ResultOfMethodCallIgnored
+     */
     public static String getOutputDirectory(Context context) {
         File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), context.getString(R.string.app_name));
         if (!directory.exists()) {
